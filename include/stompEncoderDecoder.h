df285b21c6fb3ca42a8e10561ab38f877b8d91c8
TomL <tomlevy02@gmail.com>
2020-01-10 15:25:21 +0200

    dd

---
+++ include/stompEncoderDecoder.h
@@ -0,0 +1,23 @@
+//
+// Created by tomle on 10/01/2020.
+//
+
+#ifndef BOOST_ECHO_CLIENT_STOMPENCODERDECODER_H
+#define BOOST_ECHO_CLIENT_STOMPENCODERDECODER_H
+
+#include <vector>
+#include <string>
+#include <boost/asio.hpp>
+
+
+class stompEncoderDecoder{
+private:
+    std::vector<std::string> message;
+    std::vector<char> bytes;
+    int len;
+public:
+
+};
+
+
+#endif //BOOST_ECHO_CLIENT_STOMPENCODERDECODER_H
